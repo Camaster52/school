4c5eb0aefd776e2504dca53bbe9c21823bf8875b
MegaCammaster <filingleb06@gmail.com>
2025-02-14 22:02:17 +0200

    Add files via upload

---
+++ school43/forontend/JavaScript/interactive.js
@@ -0,0 +1,38 @@
+
+
+
+var openbutt = document.getElementById("openModal");
+var closebutt = document.getElementsByClassName('modal__close')[0];
+var modall = document.getElementById("myModal");
+
+openbutt.onclick = function()
+{
+    modall.style.display = "block";
+}
+
+closebutt.onclick = function()
+{
+    modall.style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
